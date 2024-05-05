7789cdc7bb0845da56c060b01bf07a41562d0caa
daaajasin <116893549+daajasin@users.noreply.github.com>
2024-05-05 16:36:35 +0000

    added a new cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,8 +1,15 @@
-import wikipedia
+#!/usr/bin/env python
 
+import wikipedia
 
 def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """Search wikipedia for names"""
+
+    results = wikipedia.search(name)
+    return results
